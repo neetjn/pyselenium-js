097fd911bf77c8246a562f9189be844ccebdca42
John Nolette <JNolette@Transparent.com>
2017-06-26 13:01:39 -0400

    Added pypi publishing.

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='pyselenium-js',
+    description='Small library with javascript utilities for official Python selenium bindings.',
+    version='1.0.0b2',
+    url='https://github.com/neetVeritas/pyselenium-js',
+    author='John Nolette',
+    author_email='john@neetgroup.net',
+    license='Apache2',
+    classifiers=[
+        'Development Status :: 2 - Beta',
+        'Intended Audience :: Quality Assurance',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 2.7'
+    ],
+    install_requires=[
+        'selenium>=3.0.0b3'
+    ]
+)
