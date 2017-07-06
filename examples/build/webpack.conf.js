1fed1d0e8ee1452856b6906daae3ec07d56acb58
jnolette <jnolette@transparent.com>
2017-07-05 20:19:36 -0400

    Removed build from ignore

---
+++ examples/build/webpack.conf.js
@@ -0,0 +1,49 @@
+const path = require('path');
+
+function resolve (dir) {
+  return path.join(__dirname, '..', dir)
+};
+
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = {
+  entry: {
+      app: './src/main.js'
+  },
+  output: {
+    path: resolve('./dist'),
+    publicPath: 'dist/',
+    filename: 'dist.js'
+  },
+  resolve: {
+    extensions: ['.js', '.json', '.scss', '.html'],
+    alias: {
+      '@': resolve('src'),
+      'styles': resolve('src/assets/styles'),
+      'scss-loader': 'sass-loader'
+    }
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        include: [resolve('src')]
+      },
+      {
+        test: /\.s[a|c]ss$/,
+        loader: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: 'css-loader!sass-loader'
+        })
+      },
+      { 
+        test: /\.html$/, 
+        use: ['raw-loader']
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin('dist.css'),
+  ]
+};
