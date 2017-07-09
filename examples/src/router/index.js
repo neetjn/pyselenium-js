54f16bbc5bbd469a894645b3987fe3363787ebc5
John Nolette <john@neetgroup.net>
2017-07-09 10:54:16 -0400

    Included form, mixin

--- examples/src/router/index.js
+++ examples/src/router/index.js
@@ -1,42 +1,20 @@
+import homeCtrl from '@/router/routes/home/controller.js'
+import aboutCtrl from '@/router/routes/about/controller.js'
+
 export default ['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
 
   $urlRouterProvider.otherwise('/')
 
   $stateProvider.state('home', {
     url: '/',
-    template: require('angularjs-template-loader!@/views/home.html'),
-    controller: ['$scope', function($scope) {
-      $scope.author = {
-        fname: 'John',
-        lname: 'Nolette',
-        age: 21
-      }
-      $scope.users = [
-        {
-          fname: 'John',
-          lname: 'Doe',
-          age: 21,
-          field: 'Compuer Science'
-        },
-        {
-          fname: 'Jane',
-          lname: 'Doe',
-          age: 22,
-          field: 'Finance'
-        },
-        {
-          fname: 'Foo',
-          lname: 'Bar',
-          age: 26,
-          field: 'Liberal Arts'
-        }
-      ]
-    }]
+    template: require('angularjs-template-loader!@/router/routes/home/home.html'),
+    controller: homeCtrl
   })
 
   $stateProvider.state('about', {
     url: '/about',
-    template: require('angularjs-template-loader!@/views/about.html')
+    template: require('angularjs-template-loader!@/router/routes/about/about.html'),
+    controller: aboutCtrl
   })
 
 }]
