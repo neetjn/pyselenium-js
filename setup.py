46423c9a1add21b2e97d52f2cefd01d862382b43
John Nolette <JNolette@Transparent.com>
2017-08-21 13:43:02 -0400

    Updated version, package classifiers.

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup, find_packages
 setup(
     name='pyseleniumjs',
     description='Small library with javascript utilities for official Python selenium bindings.',
-    version='1.1.1',
+    version='1.1.2b',
     url='https://github.com/neetVeritas/pyselenium-js',
     author='John Nolette',
     author_email='john@neetgroup.net',
     license='Apache2',
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
+        'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python :: 2.7'
