2d64ad7ac093638db0d19da858f6bb4d4cfbcfa2
John Nolette <john@neetgroup.net>
2017-08-24 11:45:54 -0400

    Fixed depracation warning

--- pyseleniumjs/e2ejs.py
+++ pyseleniumjs/e2ejs.py
@@ -18,6 +18,7 @@
 import uuid
 import re
 import json
+import warnings
 
 class E2EJS(object):
     """
@@ -294,11 +295,13 @@ class E2EJS(object):
     def trigger_keypress(self, element, key_code):
         """
         :Description: Trigger specific key "keypress" event on given element.
+        :Warning: This method will be deprecated in version 2, use trigger_event.
         :param element: Element for browser instance to target.
         :type element: WebElement
         :param key_code: Code of key to invoke event.
         :type key_code: int
         """
+        warnings.warn('Deprecated in version 2 switch to `trigger_event`', UserWarning)
         self.browser.execute_script(
             'var e = new Event("KeyboardEvent"); e.initEvent("keypress", true, true); \
             e.which=%s; arguments[0].dispatchEvent(e);' % key_code,
